593b68eba8416cd2462d02657ba9863f142c7d30
PhVHoang <a2k40pbc@gmail.com>
2022-05-09 10:30:57 +0900

    update log_path
    Signed-off-by: PhVHoang <a2k40pbc@gmail.com>

--- distcp_subprocess.py
+++ distcp_subprocess.py
@@ -1,5 +1,13 @@
 import subprocess
 from subprocess import check_call
 
-def distcp(from_path: str, to_path: str) -> None:
-    check_call(['hadoop', 'distcp', from_path, to_path], stderr=subprocess.STDOUT)
+from typing import Optional
+
+def distcp(from_path: str, to_path: str, log_path: Optional[str] = None) -> None:
+    """
+    Run hadoop distcp by using python subprocess.
+    """
+    if not log_path:
+        log_path = "/user/namespace1/distcp_log_test"
+
+    check_call(['hadoop', 'distcp', '-log', log_path, '-skipcrccheck', from_path, to_path], stderr=subprocess.STDOUT)
